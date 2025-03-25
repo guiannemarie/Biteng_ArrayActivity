7f3c58fb6e72ca7a784e1e022664c0a85dc97293
guiannemarie <guiannemarie9@gmail.com>
2025-03-25 22:49:39 +0800

    Create ArrayActivity.js

---
+++ ArrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Adele", "Beyoncé", "Taylor Swift"]; 
+console.log(favSingers[0]); 
+
+const favNumbers = [7, 13, 29, 42];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[1][0]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
